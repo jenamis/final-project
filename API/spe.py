4220b805b30d68a0db93796c502166cde98f5df3
Yingying Cheung <yycheungds@gmail.com>
2022-06-30 20:57:48 -0700

    incorporated updated balanced random forest model performance

--- API/spe.py
+++ API/spe.py
@@ -7,15 +7,20 @@ class spe:
         
         #if the saved_model is not empty load the saved_model to self.model
         if saved_model_filename != None:
-            self.model = pickle.load(open(saved_model_filename, 'rb'))
+            self.model = pickle.load(open(saved_model_filename),'rb')
 
     def RunPrediction(self, smiles):
         features = calculate_descriptors(smiles)
-        y = self.model.predict(features)
+        scaler = pickle.load(open('scaler.pkl', 'rb'))
+        features_scaled = scaler.transform(features)
+        y = self.model.predict(features_scaled)
+        # print(self.model)
         return y
 if __name__ == '__main__':
-    model_object = spe('API/spe_brf_model.sav')
+    model_object = spe('spe_brf_model.sav')
     smiles = "CC1CCN(CC1N(C)C2=NC=NC3=C2C=CN3)C(=O)CC#N"
+    descs = [calculate_descriptors(smiles)]
+    # print(descs)
     # print(model_object.RunPrediction([[-0.23601421, -0.23375002,  0.48955629, -0.8890365 , -0.17761245,
     #     0.1709978 , -0.90123393,  0.10786976, -0.1741022 ,  1.29234626,
     #    -0.15320403,  0.13847805,  1.66615658, -0.41771504,  0.5498574 ,
@@ -25,4 +30,4 @@ if __name__ == '__main__':
     #    -1.12285645, -0.23349792, -0.91183957, -0.95433182, -0.8439449 ,
     #     0.62572119, -0.22570688, -0.7941993 , -0.3141551 ,  0.6681677 ,
     #    -0.10242044, -0.20506081, -0.59315847, -0.64066596, -1.15652533]]))
-    model_object.RunPrediction(smiles)
+    print(model_object.RunPrediction(smiles))
