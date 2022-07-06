d1180e36565a1cb308d9b8230f4f2d481912f4ad
Luke Perrin <97007592+luperrin@users.noreply.github.com>
2022-07-05 19:57:48 -0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,52 +1,70 @@
-# import os
-# os.system("pip install -r requirements.txt")
-import time
+from purifai.methodSelection import model_selection
 import pandas as pd
-# from lcms_ml_functions import *
 from graphical_interface import gui_input_file, gui_outputfile_file
-from methodPredictor import RunSPEPrediction, RunLCMSPrediction
-from chemCalculate import calculate_descriptors
+from contextlib import redirect_stdout
+import warnings
+from datetime import datetime
 
 
-if __name__ == "__main__":
+def predict_methods_from_smiles(self):
 
-    print("PurifAI Application Initialzing.......")
+    model_predictor = model_selection("static/models/spe_brf_model.pkl",
+                                "static/models/spe_scaler.pkl",
+                                "static/models/lcms_brf_model.pkl",
+                                "static/models/lcms_scaler.pkl")
 
-    # inputfile = gui_input_file()
-    inputfile = "/Users/lukeperrin/Downloads/smiles_list.csv"
-    df = pd.read_csv(inputfile)
+    predicted_spe_model = model_predictor.RunSPEPrediction(self)
+    predicted_lcms_model = model_predictor.RunLCMSPrediction(self)
+    return predicted_spe_model, predicted_lcms_model
+
+def evaluate_structures(self):
+
+    df = pd.read_csv(self)
     df = df.dropna(subset=['SMILES'])
-    print(df)
-    smiles = df['SMILES'].to_list()
-
 
     df["PREDICTED_SPE_METHOD"] = ""
     df["PREDICTED_LCMS_METHOD"] = ""
 
-    for i in range(len(df)):
+    N = len(df['SMILES'])
+
+    for i in range(N):
+
         smiles = df.loc[i, 'SMILES']
-        print(smiles)
-        predicted_SPE_method = RunSPEPrediction(smiles)
-        df.loc[i, "PREDICTED_SPE_METHOD"] = str(predicted_SPE_method)
-        print("RunSPEPrediction succesful...")
-        predicted_LCMS_method = RunLCMSPrediction(smiles)
-        df.loc[i, "PREDICTED_LCMS_METHOD"] = str(predicted_LCMS_method)
 
-    # Generate structure data (features)
-    descriptors_results = []
-    for smile in smiles:
-        descriptors = calculate_descriptors(smile)
-        descriptors_results.append(descriptors)
-        print(descriptors)
-    print(descriptors_results)
-    names = ['MolWt', 'ExactMolWt', 'qed', 'TPSA', 'HeavyAtomMolWt', 'MolLogP', 'MolMR', 'FractionCSP3', 'NumValenceElectrons', 'MaxPartialCharge', 'MinPartialCharge', 'FpDensityMorgan1', 'BalabanJ', 'BertzCT', 'HallKierAlpha', 'Ipc', 'Kappa2', 'LabuteASA', 'PEOE_VSA10', 'PEOE_VSA2', 'SMR_VSA10', 'SMR_VSA4', 'SlogP_VSA2', 'SlogP_VSA6','MaxEStateIndex', 'MinEStateIndex', 'EState_VSA3', 'EState_VSA8', 'HeavyAtomCount', 'NHOHCount', 'NOCount', 'NumAliphaticCarbocycles', 'NumAliphaticHeterocycles', 'NumAliphaticRings', 'NumAromaticCarbocycles', 'NumAromaticHeterocycles', 'NumAromaticRings', 'NumHAcceptors', 'NumHDonors', 'NumHeteroatoms', 'NumRotatableBonds', 'NumSaturatedCarbocycles', 'NumSaturatedHeterocycles', 'NumSaturatedRings', 'RingCount']
-    descriptors_df = pd.DataFrame(descriptors_results, columns=names)
-    print(descriptors_df)
+        result = predict_methods_from_smiles(smiles)
 
-    descriptors_df['SMILES'] = df['SMILES']
-    result_df = pd.concat([df, descriptors_df], axis=1)
-    print(result_df)
-    # outputfile = gui_outputfile_file()
-    outputfile = "/Users/lukeperrin/Downloads/predictor_results.csv"
-    result_df.to_csv(outputfile, index=False)
+        predicted_SPE_method = result[0]
+        df.loc[i, "PREDICTED_SPE_METHOD"] = predicted_SPE_method
 
+        predicted_LCMS_method = result[1]
+        df.loc[i, "PREDICTED_LCMS_METHOD"] = predicted_LCMS_method
+
+    return df
+
+
+if __name__ == "__main__":
+
+    timestamp = datetime.now()
+
+    timestamp = str(timestamp).replace(" ", "_@T=")
+
+    with open(f"log_outfiles/{timestamp}_log.txt", 'w') as f:
+
+        print("File began running:")
+        print(timestamp)
+
+        with redirect_stdout(f):
+
+            print('data')
+
+            warnings.filterwarnings("ignore")
+
+            file_select = gui_input_file()
+            predictions_df = evaluate_structures(file_select)
+
+            try:
+                predictions_df.to_csv(file_select, index=False)
+                print("Application completed without errors.")
+
+            except:
+                print("Application completed with errors.")
